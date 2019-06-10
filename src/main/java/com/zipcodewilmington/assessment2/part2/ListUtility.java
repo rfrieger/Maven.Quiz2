2ae07e09b654c5bb389360b7f7c13073ac59ba59
Robert Rieger <robertrieger@zipcoders-MacBook-Pro-96.local>
2019-06-10 12:26:10 -0400

    57 of 89 pssing compiles

--- src/main/java/com/zipcodewilmington/assessment2/part2/ListUtility.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/ListUtility.java
@@ -1,18 +1,24 @@
 package com.zipcodewilmington.assessment2.part2;
 
+import com.sun.xml.internal.ws.api.ha.StickyFeature;
+
 import javax.rmi.CORBA.Util;
 import java.util.ArrayList;
 import java.util.List;
 
 public class ListUtility{
+    ArrayList<Object> arr = new ArrayList<>();
 
     public Boolean add(int i) {
+        arr.add(i);
+        if (arr.contains(i)) {
+            return true;
+        } else return false;
 
-      return null;
     }
 
     public Integer size() {
-       return null;
+       return arr.size();
     }
 
     public List<Integer> getUnique() {
