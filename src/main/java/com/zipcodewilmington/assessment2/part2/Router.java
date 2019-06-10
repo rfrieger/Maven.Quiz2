5c420114437c1ea220d45dde2c3b2b2896f28741
Robert Rieger <robertrieger@zipcoders-MacBook-Pro-96.local>
2019-06-10 13:26:10 -0400

    add

--- src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
+++ src/main/java/com/zipcodewilmington/assessment2/part2/Router.java
@@ -1,26 +1,30 @@
 package com.zipcodewilmington.assessment2.part2;
 
 import java.util.HashMap;
+import java.util.HashSet;
 
 public class Router {
-
+    HashMap<String, String> hm = new HashMap<>();
 
 
     public void add(String path, String controller) {
+        hm.put(path,controller);
     }
 
     public Integer size() {
-        return null;
+        return hm.size();
     }
 
 
     public String getController(String path) {
-        return null;
+        return hm.get(path);
     }
 
     public void update(String path, String studentController) {
+        hm.replace(path, studentController);
     }
 
     public void remove(String path) {
+        hm.remove(path);
     }
 }
